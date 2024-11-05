06f0b423f7f1d29c216fccd08f04c211066d5b18
Andrea Botello <andrea.botellor@misena.edu.co>
2024-11-05 11:51:09 -0500

    Entrega

--- src/config/global.js
+++ src/config/global.js
@@ -172,7 +172,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/CF2_13410562_DU.zip',
+        download: 'downloads/CF2_13410562_DU.pdf',
       },
       {
         icono: 'fas fa-download',
